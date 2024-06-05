2a6bbabfe2336796ff5952dc64d9db8cfdac49cc
83771UEP <b.wojtas1@gmail.com>
2024-06-05 23:21:04 +0200

    .

--- raport_functions.py
+++ raport_functions.py
@@ -1,6 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+# Standard libraries
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+
+import math
+import logging
+from typing import Tuple, Optional, Dict, Any
+
+# Data handling libraries
+import numpy as np
+import pandas as pd
+
+# Visualization libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.neighbors import KNeighborsClassifier
+# Scikit-learn libraries
+from sklearn import datasets, ensemble
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, median_absolute_error, accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import GridSearchCV, KFold, cross_validate, train_test_split, RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB, BernoulliNB, CategoricalNB
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.svm import SVC
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.impute import SimpleImputer
+
+# Imbalanced-learn libraries
+from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.combine import SMOTETomek, SMOTEENN
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_starting_data():
     data = pd.read_csv('winequality-red.csv', sep=";")
@@ -45,3 +79,249 @@ def corr_matrix(data):
     plt.title('Numerical Features Correlation')
     plt.show()
 
+def gaussian_naive_bayes_main():
+    try:
+        
+        #Podział danych na zbior testowy i treningowy
+        X_train, X_test, y_train, y_test = initialize_and_split_data("minmax", 'none')
+
+        #Utworzenie obiektu modelu
+        gnb = GaussianNB()
+
+        #Wytrenowanie i wstepna ocena modelu
+        best_params = {}
+        cv_results, accuracy_without_optimization = cross_validation_and_evaluation(X_train, y_train, gnb, best_params)
+
+        #Hiperparametryzacja
+        param_grid = {
+            'var_smoothing': np.logspace(0, -9, num=100)
+        }
+        estimator = GaussianNB()
+        best_params = optimize_hyperparameters(estimator, X_train, y_train, param_grid)
+
+        #Walidacja krzyżowa modelu ze zoptymalizowanymi hiperparametrami
+        cv_resultsOPT, accuracy_with_optimization = cross_validation_and_evaluation(X_train, y_train, gnb, best_params)
+
+        #Ponowne wyktrenowanie modelu, ale ze zoptymalizowanymi hiperparametrami
+        gnb.set_params(**best_params)
+        gnb.fit(X_train, y_train)
+
+        #Predykcje na zbiorze testowym
+        y_pred_optimized = gnb.predict(X_test)
+
+        #Generacja raportu dla modelul
+        report_optimized = generate_report(y_test, y_pred_optimized)
+
+        #Generacja wykresów dla modelu
+        display_plots(y_test, y_pred_optimized, report_optimized, cv_resultsOPT, accuracy_without_optimization, accuracy_with_optimization)
+
+    except Exception as e:
+        logging.error("An error occurred during model evaluation and plotting.", exc_info=True)
+
+
+def initialize_data(scaler_type: Optional[str] = None) -> Tuple[pd.DataFrame, pd.Series]:
+    
+    # Pobranie danych z plików .csv
+    X = pd.read_csv('wineData.csv')
+    y = pd.read_csv('wineRatings.csv')
+    y = y.values.ravel()
+    
+    #Informacje o danych do logów
+
+    #Przeskalowanie danych przy użyciu obiektu MinMaxScaler
+    if scaler_type in ["minmax", "standard"]:
+        scaler = MinMaxScaler() if scaler_type == "minmax" else StandardScaler()
+        X = scaler.fit_transform(X)
+    else:
+        logging.info("No scaling applied.")
+
+    return X, y
+
+#Funkcja definiująca strategię próbkowania
+def sampling_strategy(y):
+    unique, counts = np.unique(y, return_counts=True)
+    max_count = max(counts)
+    return {class_label: max(int(0.40 * max_count), count) for class_label, count in zip(unique, counts)}
+
+#Funkcja dzieląca dane na zbiór treningowy i testowy oraz nakladająca oversampler
+def initialize_and_split_data(scaler_type: str, method: str = 'none') -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    X, y = initialize_data(scaler_type)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    if method == 'smote':
+        strategy = sampling_strategy(y_train)
+        oversampler = SMOTE(sampling_strategy=strategy, k_neighbors=2, random_state=42)
+        X_train, y_train = oversampler.fit_resample(X_train, y_train)
+    return X_train, X_test, y_train, y_test
+
+#Funkcja ewaluująca model
+def cross_validation_and_evaluation(X_train: np.ndarray, y_train: np.ndarray, model: Any, best_params: Dict[str, Any], cv_splits: int = 5, random_state: int = 42) -> Tuple[float, np.ndarray, np.ndarray, np.ndarray, list, Dict[str, Any]]:
+    # Set the best parameters for the model
+    model.set_params(**best_params)
+    pipe = make_pipeline(model)
+
+    # Perform cross-validation
+    kfold = KFold(n_splits=cv_splits, shuffle=True, random_state=random_state)
+    cv_results = cross_validate(pipe, X_train, y_train, cv=kfold, return_train_score=True)
+    return cv_results, cv_results['test_score'].mean()
+
+#Funkcja znajująca najlepsze hiperparametry
+def optimize_hyperparameters(estimator, X_train: np.ndarray, y_train: np.ndarray, param_grid: Dict[str, Any], n_iter: int = 50, cv_splits: int = 5, random_state: int = 42) -> Dict[str, Any]:
+    random_search = RandomizedSearchCV(estimator=estimator, param_distributions=param_grid, n_iter=n_iter, cv= cv_splits, scoring='accuracy', random_state=random_state, n_jobs=-1, verbose=2)
+    random_search.fit(X_train, y_train)
+    logging.info(f"Best parameters: {random_search.best_params_}")
+    return random_search.best_params_
+
+def generate_report(y_test: Any, y_pred: Any) -> Dict[str, Any]:
+    # Determine unique classes
+    classes = [3, 4, 5, 6, 7, 8]
+
+    report = classification_report(y_test, y_pred, labels=classes, output_dict=True, zero_division=1)
+    logging.info("Classification report:\n%s", classification_report(y_test, y_pred, labels=classes, zero_division=1))
+    return report
+
+#Funkcja generująca histogram
+def plot_histogram(y_test: Any, y_pred: Any, ax: plt.Axes) -> None:
+    pd.DataFrame({'Actual': y_test, 'Predicted': y_pred}).plot.hist(ax=ax, alpha=0.5)
+    ax.set_title('Actual vs Predicted values')
+    ax.set_xlabel('Values')
+    ax.set_ylabel('Frequency')
+    ax.legend(['Actual', 'Predicted'])
+
+#Funkcja generująca wykres przewidywań vs właściwych ocen wina
+def plot_actual_vs_predicted(y_test: Any, y_pred: Any, ax: plt.Axes) -> None:
+    pd.DataFrame({'Actual': y_test, 'Predicted': y_pred}).plot(ax=ax, marker='o')
+    ax.set_title('Actual vs Predicted values')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Values')
+    ax.legend(['Actual', 'Predicted'])
+
+#Funkcja generująca raport klasyfikacji
+def plot_classification_report(report: Dict[str, Any], ax: plt.Axes) -> None:
+    report_df = pd.DataFrame(report).transpose().drop(columns='support')
+    report_df.plot(kind='bar', ax=ax)
+    ax.set_title('Classification Report Metrics')
+    ax.set_xlabel('Metrics')
+    ax.set_ylabel('Score')
+    ax.set_ylim(0, 1)
+    ax.legend(loc='lower right')
+
+#Funkcja generująca macierz pomyłek
+def plot_confusion_matrix(report: np.ndarray, classes: list, ax: plt.Axes) -> None:
+    sns.heatmap(report, annot=True, fmt='d', xticklabels=classes, yticklabels=classes, ax=ax, cmap='Blues')
+    ax.set_title('Confusion Matrix')
+    ax.set_xlabel('Predicted')
+    ax.set_ylabel('Actual')
+
+#Funkcja wywołująca wszystkie wykesy
+def display_plots(y_fold_test: np.ndarray, y_pred: np.ndarray, report: Dict[str, Any], cv_results: Dict[str, Any], accuracy_without_optimization: float, accuracy_with_optimization: float) -> None:
+    fig, axs = plt.subplots(3, 2, figsize=(16, 16))
+    classes = [3, 4, 5, 6, 7, 8]
+    plot_actual_vs_predicted(y_fold_test, y_pred, axs[0, 0])
+    plot_confusion_matrix(confusion_matrix(y_fold_test, y_pred, labels=classes), classes, axs[0, 1])
+    plot_classification_report(report, axs[1, 0])
+    plot_histogram(y_fold_test, y_pred, axs[2, 1])
+    
+    axs[1, 1].plot(cv_results['test_score'], label='Test Score')
+    axs[1, 1].plot(cv_results['train_score'], label='Train Score')
+    axs[1, 1].set_title('Cross-validation Results with Optimized Hyperparameters')
+    axs[1, 1].set_xlabel('Fold Index')
+    axs[1, 1].set_ylabel('Accuracy')
+    axs[1, 1].legend()
+
+    axs[2, 0].bar(['Without Optimization', 'With Optimization'], [accuracy_without_optimization, accuracy_with_optimization])
+    axs[2, 0].set_title('Comparison of Model Performance')
+    axs[2, 0].set_ylabel('Mean Accuracy Score')
+    
+    plt.tight_layout()
+    plt.show()
+
+def knn_main():
+    try:
+        #Podział danych na zbior testowy i treningowy
+        X_train, X_test, y_train, y_test = initialize_and_split_data("minmax", 'smote')
+        logging.info("Data initialization and splitting complete.")
+
+        #Utworzenie obiektu modelu
+        knn = KNeighborsClassifier()
+
+        #Wytrenowanie i wstepna ocena modelu
+        best_params = {}
+        cv_results, accuracy_without_optimization = cross_validation_and_evaluation(X_train, y_train, knn, best_params)
+
+        #Hiperparametryzacja
+        param_grid = {
+            'n_neighbors': range(1, 21),
+            'weights': ['uniform', 'distance'],
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+            'p': [1, 2]
+        }
+        estimator = KNeighborsClassifier()
+        best_params = optimize_hyperparameters(estimator, X_train, y_train, param_grid)
+
+        #Walidacja krzyżowa modelu ze zoptymalizowanymi hiperparametrami
+        cv_resultsOPT, accuracy_with_optimization = cross_validation_and_evaluation(X_train, y_train, knn, best_params)
+
+        #Ponowne wyktrenowanie modelu, ale ze zoptymalizowanymi hiperparametrami
+        knn.set_params(**best_params)
+        knn.fit(X_train, y_train)
+
+        #Predykcje na zbiorze testowym
+        y_pred_optimized = knn.predict(X_test)
+
+        #Generacja raportu dla modelu
+        report_optimized = generate_report(y_test, y_pred_optimized)
+
+        #Generacja wykresów dla modelu
+        display_plots(y_test, y_pred_optimized, report_optimized, cv_resultsOPT, accuracy_without_optimization, accuracy_with_optimization)
+
+    except Exception as e:
+        logging.error("An error occurred during model evaluation and plotting.", exc_info=True)
+
+
+def random_forest_main():
+    try:
+        #Podział danych na zbior testowy i treningowy
+        X_train, X_test, y_train, y_test = initialize_and_split_data("standard", 'smote')
+        logging.info("Data initialization and splitting complete.")
+
+        #Utworzenie obiektu modelu
+        rf = RandomForestClassifier()
+
+        #Wytrenowanie i wstepna ocena modelu
+        best_params = {}
+        cv_results, accuracy_without_optimization = cross_validation_and_evaluation(X_train, y_train, rf, best_params)
+
+        #Hiperparametryzacja
+        param_grid = {
+            'n_estimators': np.arange(50, 501, 50),  # number of trees in the forest
+            'max_depth': [None] + list(np.arange(10, 110, 10)),  # maximum depth of the tree
+            'min_samples_split': np.arange(2, 12, 2),  # minimum number of samples required to split an internal node
+            'min_samples_leaf': np.arange(1, 12, 2),  # minimum number of samples required to be at a leaf node
+            'max_features': ['sqrt', 'log2'],  # number of features to consider when looking for the best split
+            'bootstrap': [True, False]  # whether bootstrap samples are used when building trees
+        }
+        estimator = RandomForestClassifier()
+        best_params = optimize_hyperparameters(estimator, X_train, y_train, param_grid)
+
+        #Walidacja krzyżowa modelu ze zoptymalizowanymi hiperparametrami
+        cv_resultsOPT, accuracy_with_optimization = cross_validation_and_evaluation(X_train, y_train, rf, best_params)
+
+        #Ponowne wyktrenowanie modelu, ale ze zoptymalizowanymi hiperparametrami
+        rf.set_params(**best_params)
+        rf.fit(X_train, y_train)
+
+        #Predykcje na zbiorze testowym
+        y_pred_optimized = rf.predict(X_test)
+
+        #Generacja raportu dla modelu
+        report_optimized = generate_report(y_test, y_pred_optimized)
+
+        #Generacja wykresów dla modelu
+        display_plots(y_test, y_pred_optimized, report_optimized, cv_resultsOPT, accuracy_without_optimization, accuracy_with_optimization)
+
+        #generacja wizualizacji lasu losowego
+        #Można odkomentować jeśli nie zależy na czasie (UWAGA: mi zawiesiło komputer)
+        plot_forest(rf)
+
+    except Exception as e:
+        logging.error("An error occurred during model evaluation and plotting.", exc_info=True)
